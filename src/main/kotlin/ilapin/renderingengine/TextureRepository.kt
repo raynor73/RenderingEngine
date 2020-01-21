051ecc3bb2d55ac399ca25bd11567246e607dfbb
Igor Lapin <raynor73@gmail.com>
2020-01-22 00:37:11 +0300

    Add texture for rendering creation method declaration

--- src/main/kotlin/ilapin/renderingengine/TextureRepository.kt
+++ src/main/kotlin/ilapin/renderingengine/TextureRepository.kt
@@ -2,6 +2,8 @@ package ilapin.renderingengine
 
 interface TextureRepository {
 
+    fun createTextureForRendering(textureName: String, width: Int, height: Int)
+
     fun createTexture(textureName: String, width: Int, height: Int, data: IntArray)
 
     fun deleteTexture(textureName: String)
