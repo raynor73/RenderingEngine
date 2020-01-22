cde0dfba5ea8a8b5a47d48b75e8dc2656a956393
Igor Lapin <raynor73@gmail.com>
2020-01-23 00:12:42 +0300

    Add texture for rendering depth creation method declaration

--- src/main/kotlin/ilapin/renderingengine/TextureRepository.kt
+++ src/main/kotlin/ilapin/renderingengine/TextureRepository.kt
@@ -2,6 +2,8 @@ package ilapin.renderingengine
 
 interface TextureRepository {
 
+    fun createTextureForRenderingDepth(textureName: String, width: Int, height: Int)
+
     fun createTextureForRendering(textureName: String, width: Int, height: Int)
 
     fun createTexture(textureName: String, width: Int, height: Int, data: IntArray)
